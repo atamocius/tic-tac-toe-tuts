6ca561e950cda8071747a98c8459be9bdd609233
Daniel Anton Mata <anton.mata@gmail.com>
2021-04-21 06:14:39 +0800

    Cleaned up the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Game from "./game";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
